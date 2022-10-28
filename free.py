9d66ddbd133ede081c4674400ad4fe6e2f78c435
Toxic-Ayan <114830936+Toxic-Ayan@users.noreply.github.com>
2022-10-28 22:47:50 +0600

    Add files via upload

---
+++ free.py
@@ -0,0 +1,18 @@
+import os
+os.system("clear")
+
+print(" \033[1;31m==>FREE SCRIPT COLLECT MY GITHUB REPOSITORIES ")
+print(" \033[1;32mFILE NAME =>[ FREE-SCRIPT ] ")
+ 
+print(" ==>HOWDY\033[1;31m  ITS\033[1;32m AYAN\033[1;33m  SAGOR ")
+print(" [1] FOLLOW\033[1;32m MY\033[1;34m  GITHUB\033[1;33m UNLOCK\033[1;37m SCRIPT ")
+print(" [2] JOIN\033[1;38m GROUP\033[1;39m GET\033[1;34m FREE\033[1;36m SCRIPT ")
+
+
+ayan = input ( 'choose>===>> ' )
+if ayan in ['1']:
+	os.system('xdg-open https://github.com/Toxic-Ayan')
+if ayan in ['2']:
+		os.system('xdg-open https://facebook.com/groups/1215404685982268/')
+		
+		
